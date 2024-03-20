19a229f7e589619be0062cc8a35949209501e8be
camilo1962 <108455047+camilo1962@users.noreply.github.com>
2024-03-20 09:30:32 +0100

    Initial commit

---
+++ Assets/Scripts/pieza.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class pieza : MonoBehaviour
+{
+    private Vector3 PosicionCorrecta;
+    public bool Encajada;
+    public bool Seleccionada;
+
+    void Start()
+    {
+        PosicionCorrecta = transform.position;
+        transform.position = new Vector3(Random.Range(5f,14f), Random.Range(3f,-8f));
+    }
+    
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, PosicionCorrecta) < 0.5f)
+        {
+            if (!Seleccionada)
+            {
+                if (Encajada == false)
+                {
+                    transform.position = PosicionCorrecta;
+                    Encajada = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                    Camera.main.GetComponent<juego>().PiezasEncajadas++;
+                }
+            }
+        }
+    }
+}
